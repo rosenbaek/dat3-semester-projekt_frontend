735d4d9e3f0f5160e829f76dc770424f40b55d2a
rosenbaek <59441104+rosenbaek@users.noreply.github.com>
2021-11-26 13:00:52 +0100

    Added table

--- src/components/TotalPortfolioComponent.js
+++ src/components/TotalPortfolioComponent.js
@@ -1,5 +1,6 @@
 import { styled, useTheme } from "@mui/material/styles";
-import { Card, Typography } from "@mui/material";
+import { Card, Typography, Box, Grid } from "@mui/material";
+import ReactApexChart from "react-apexcharts";
 
 const RootStyle = styled(Card)(({ theme }) => ({
 	boxShadow: "none",
@@ -7,18 +8,91 @@ const RootStyle = styled(Card)(({ theme }) => ({
 	borderRadius: 16,
 	padding: theme.spacing(5, 0),
 	color: "rgb(0, 82, 73)",
-	backgroundColor: "#C8FACD",
+	backgroundColor: "#F7FAFA",
+	borderWidth: 1,
+	borderStyle: "solid",
+	borderColor: "lightgrey",
 }));
+
+const CHART_DATA = [
+	{
+		name: "Team B",
+		type: "area",
+		data: [44, 55, 41, 67, 22, 43, 21, 41, 56, 27, 43],
+	},
+];
 
 export default function TotalPortfolioComponent({ value, currency }) {
 	const theme = useTheme();
+
+	const chartOptions = {
+		stroke: {
+			width: 2,
+			curve: "smooth",
+			lineCap: "round",
+		},
+
+		plotOptions: { bar: { columnWidth: "11%", borderRadius: 4 } },
+		fill: {
+			type: "gradient",
+			gradient: {
+				type: "vertical",
+				shadeIntensity: 0,
+				opacityFrom: 0.4,
+				opacityTo: 0,
+				stops: [0, 100],
+			},
+		},
+
+		labels: [
+			"01/01/2003",
+			"02/01/2003",
+			"03/01/2003",
+			"04/01/2003",
+			"05/01/2003",
+			"06/01/2003",
+			"07/01/2003",
+			"08/01/2003",
+			"09/01/2003",
+			"10/01/2003",
+			"11/01/2003",
+		],
+		chart: {
+			toolbar: { show: false },
+			zoom: { enabled: false },
+			// animations: { enabled: false },
+			foreColor: theme.palette.text.disabled,
+			fontFamily: theme.typography.fontFamily,
+		},
+
+		colors: ["blue"],
+		xaxis: { type: "datetime" },
+	};
+
 	return (
 		<RootStyle theme={theme}>
-			<Typography variant="h5">Total Porfolio value</Typography>
-			<Typography variant="h3">{value}</Typography>
-			<Typography variant="subtitle2" sx={{ opacity: 0.72 }}>
-				{currency}
-			</Typography>
+			<Grid container spacing={1}>
+				{/* on small screens(phones), this item takes up 5 units. On medium-sized screens(tablets), take up 8 units. If an item is too large, it will go to the next line. */}
+				<Grid item xs>
+					<Typography variant="h5" sx={{ float: "left", marginLeft: 4 }}>
+						Total Porfolio value
+					</Typography>
+				</Grid>
+				<Grid item xs>
+					<Typography variant="h5" sx={{ float: "right", marginRight: 4 }}>
+						{value} {currency}
+					</Typography>
+				</Grid>
+			</Grid>
+
+			<Box sx={{ p: 3, pb: 1 }} dir="ltr">
+				<ReactApexChart
+					type="line"
+					series={CHART_DATA}
+					options={chartOptions}
+					height={364}
+				/>
+			</Box>
 		</RootStyle>
 	);
 }
