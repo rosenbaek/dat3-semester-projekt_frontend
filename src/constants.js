23c20f0e41910d7826d30c6facfd258f1f7319b5
rosenbaek <59441104+rosenbaek@users.noreply.github.com>
2021-12-02 09:56:34 +0100

    small change

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,3 @@
 export const URL = "http://localhost:8080/stockmonitor_backend";
+//export const URL = "https://itpiloten.dk/tomcat/stockmonitor_backend";
 export const URL_STOCK_API = URL + "/api/stock";
