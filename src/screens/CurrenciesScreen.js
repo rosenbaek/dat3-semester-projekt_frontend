b7a537f00260d6049a68b73f272a55aa4747e4db
rosenbaek <59441104+rosenbaek@users.noreply.github.com>
2021-12-08 19:04:27 +0100

    styling and add base currency

--- src/screens/CurrenciesScreen.js
+++ src/screens/CurrenciesScreen.js
@@ -3,7 +3,7 @@ import { DataGrid, GridActionsCellItem } from "@mui/x-data-grid";
 import { useEffect, useState } from "react";
 import StockFacade from "../facades/StockFacade";
 import DeleteIcon from "@mui/icons-material/Delete";
-import { Typography, Box } from "@mui/material";
+import { Typography, Box, Autocomplete, TextField } from "@mui/material";
 
 const APP_BAR_MOBILE = 64;
 const APP_BAR_DESKTOP = 92;
@@ -29,6 +29,7 @@ var columns = [
 	},
 	{
 		field: "name",
+		headerName: "Name",
 		flex: 1,
 		minWidth: 140,
 	},
@@ -40,15 +41,16 @@ var columns = [
 	},
 	{
 		field: "Day7",
+		headerName: "Last 7 days",
 		flex: 1,
 		type: "number",
 		minWidth: 100,
 		renderCell: (params) => (
 			<Box
 				sx={{
-					backgroundColor: params.value > 0 ? "green" : "red",
+					backgroundColor: params.value > 0 ? "#F74769" : "#0EC477",
 					color: "white",
-					borderRadius: 5,
+					borderRadius: 3,
 					padding: 1,
 					height: 35,
 					display: "flex",
@@ -64,14 +66,15 @@ var columns = [
 	{
 		field: "Day14",
 		flex: 1,
+		headerName: "Last 14 days",
 		minWidth: 100,
 		type: "number",
 		renderCell: (params) => (
 			<Box
 				sx={{
-					backgroundColor: params.value > 0 ? "green" : "red",
+					backgroundColor: params.value > 0 ? "#F74769" : "#0EC477",
 					color: "white",
-					borderRadius: 5,
+					borderRadius: 3,
 					padding: 1,
 					height: 35,
 					display: "flex",
@@ -87,14 +90,15 @@ var columns = [
 	{
 		field: "LastMonth",
 		flex: 1,
+		headerName: "Last Month",
 		minWidth: 100,
 		type: "number",
 		renderCell: (params) => (
 			<Box
 				sx={{
-					backgroundColor: params.value > 0 ? "green" : "red",
+					backgroundColor: params.value > 0 ? "#F74769" : "#0EC477",
 					color: "white",
-					borderRadius: 5,
+					borderRadius: 3,
 					padding: 1,
 					height: 35,
 					display: "flex",
@@ -108,43 +112,37 @@ var columns = [
 		),
 	},
 ];
-const Test = (val) => {
-	return <Typography sx={{ backgroundColor: "red" }}>{val}</Typography>;
-};
+
 const CurrenciesScreen = ({ user }) => {
-	const [rows, setRows] = useState([
-		{
-			id: 1,
-			Code: "yer",
-			Name: "Yemeni rial",
-			Value: 0.0,
-		},
-		{
-			id: 2,
-			Code: "zar",
-			Name: "South African rand",
-			Value: 15.959,
-		},
-	]);
+	const [baseCurrency, setBaseCurrency] = useState("");
+	const [rows, setRows] = useState([]);
+
+	useEffect(() => {
+		console.log(baseCurrency);
+	});
 
 	useEffect(() => {
 		if (user) {
-			StockFacade.getAllHistoricalCurrencies(user.defaultCurrency, (res) => {
-				setRows(
-					res.map((c, index) => {
-						c.id = index;
-						c.Code = c.code.toUpperCase();
-						c.Current = c.value.toFixed(2);
-						c.LastMonth = calcPercent(c.value, c.lastMonth).toFixed(2);
-						c.Day7 = calcPercent(c.value, c.day7).toFixed(2);
-						c.Day14 = calcPercent(c.value, c.day14).toFixed(2);
+			if (baseCurrency === "") {
+				setBaseCurrency({ code: user.defaultCurrency });
+			} else {
+				StockFacade.getAllHistoricalCurrencies(baseCurrency.code, (res) => {
+					setRows(
+						res.map((c, index) => {
+							c.id = index;
+							c.Code = c.code.toUpperCase();
+							c.Current = c.value.toFixed(2);
+							c.LastMonth = calcPercent(c.value, c.lastMonth).toFixed(2);
+							c.Day7 = calcPercent(c.value, c.day7).toFixed(2);
+							c.Day14 = calcPercent(c.value, c.day14).toFixed(2);
 
-						return c;
-					})
-				);
-			});
+							return c;
+						})
+					);
+				});
+			}
 		}
-	}, [user]);
+	}, [user, baseCurrency]);
 
 	const calcPercent = (current, old) => {
 		const dif = current - old;
@@ -154,18 +152,57 @@ const CurrenciesScreen = ({ user }) => {
 
 	return (
 		<MainStyle>
-			<h2>Currencies</h2>
-			<h6>Base Currency: {user && user.defaultCurrency.toUpperCase()}</h6>
-			{rows ? (
-				<DataGrid
-					autoHeight
-					autoPageSize
-					rows={rows}
-					columns={columns}
-					pageSize={10}
-					rowsPerPageOptions={[10]}
-					disableColumnMenu
-				/>
+			{user ? (
+				<>
+					<Box sx={{ display: "flex", alignItems: "flex-end" }}>
+						<Box
+							sx={{
+								flex: 1,
+
+								alignItems: "flex-start",
+							}}
+						>
+							<Box sx={{ display: "flex" }}>
+								<Typography variant="h3">Currencies</Typography>
+							</Box>
+						</Box>
+						<Box sx={{ maxWidth: 200, flex: 1 }}>
+							<Autocomplete
+								options={rows.map((c) => c.Code)}
+								disablePortal
+								value={baseCurrency.code}
+								onChange={(event, code) => {
+									if (code !== null) {
+										setBaseCurrency({ code });
+									}
+								}}
+								sx={{ marginBottom: 1 }}
+								renderInput={(params) => (
+									<TextField
+										{...params}
+										id="defaultCurrency"
+										label="Base Currency"
+										InputProps={{
+											...params.InputProps,
+										}}
+									/>
+								)}
+								required
+							/>
+						</Box>
+					</Box>
+					{rows ? (
+						<DataGrid
+							autoHeight
+							autoPageSize
+							rows={rows}
+							columns={columns}
+							pageSize={10}
+							rowsPerPageOptions={[10]}
+							disableColumnMenu
+						/>
+					) : null}
+				</>
 			) : null}
 		</MainStyle>
 	);
